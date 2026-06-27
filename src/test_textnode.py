441cfe9894bc75259514300acaf3a59148beeed8
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-27 16:49:26 +0200

    feat: test capabilities with some unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,22 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self) -> None:
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+
+    def test_none(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertIsNone(node.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
