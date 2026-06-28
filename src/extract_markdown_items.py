54b2483cc0cd40b56a2f0150f5469194955897c1
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-28 16:37:06 +0200

    chore: moving comments to the high level function

--- src/extract_markdown_items.py
+++ src/extract_markdown_items.py
@@ -1,33 +1,8 @@
 import re
-
-from markdown_items import MarkdownItemType
 from textnode import TextType
 
 
 def extract_markdown_items(text: str, text_type: TextType) -> list[tuple[str, str]]:
-    if text_type == TextType.IMAGE:
-        return extract_markdown_images(text)
-    elif text_type == TextType.LINK:
-        return extract_markdown_links(text)
-    raise ValueError("Unsupported markdown item type")
-
-
-def extract_markdown_images(text: str) -> list[tuple[str, str]]:
-    """
-    Extracts image URLs and alt text from markdown text.
-
-    Args:
-        text (str): The markdown text to extract image URLs from.
-
-    Returns:
-        list[tuple[str, str]]: A list of tuples containing alt text and image URLs.
-    """
-    # Regular expression to match markdown image syntax ![alt text](image_url)
-    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    return re.findall(pattern, text)
-
-
-def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     """
     Extracts link URLs and link text from markdown text.
 
@@ -37,6 +12,20 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     Returns:
         list[tuple[str, str]]: A list of tuples containing link text and link URLs.
     """
+    if text_type == TextType.IMAGE:
+        return extract_markdown_images(text)
+    elif text_type == TextType.LINK:
+        return extract_markdown_links(text)
+    raise ValueError("Unsupported markdown item type")
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    # Regular expression to match markdown image syntax ![alt text](image_url)
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
+
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     # Regular expression to match markdown link syntax [link text](link_url)
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     return re.findall(pattern, text)
