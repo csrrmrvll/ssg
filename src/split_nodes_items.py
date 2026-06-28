07d81cea5008691680f570bdd1edc00ee5cfdb0f
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-28 16:37:29 +0200

    feat: splitting nodes with images and links

---
+++ src/split_nodes_items.py
@@ -0,0 +1,57 @@
+from split_nodes_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
+from extract_markdown_items import extract_markdown_items
+from enum import Enum
+
+
+def get_opening_symbol(text_type: TextType) -> str:
+    if text_type == TextType.IMAGE:
+        return "!["
+    elif text_type == TextType.LINK:
+        return "["
+    else:
+        raise ValueError(f"Unsupported item type: {text_type}")
+
+
+def split_nodes_items(old_nodes: list[TextNode], text_type: TextType) -> list[TextNode]:
+    """
+    Splits the text of TextNode objects into separate nodes based on markdown image and link syntax.
+
+    Args:
+        old_nodes (list[TextNode]): A list of TextNode objects to be split.
+    """
+    new_nodes: list[TextNode] = []
+    node_images = extract_markdown_items(
+        " ".join([node.text for node in old_nodes if node.text_type == TextType.TEXT]),
+        text_type,
+    )
+    if not node_images:
+        return old_nodes
+    opening_symbol = get_opening_symbol(text_type)
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            for item_text, item_link in node_images:
+                sections = node.text.split(
+                    f"{opening_symbol}{item_text}]({item_link})", 1
+                )
+                if len(sections) == 2:
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT, node.url))
+                    new_nodes.append(TextNode(item_text, text_type, item_link))
+                    node.text = sections[
+                        1
+                    ]  # Update the node text to the remaining section for further processing
+                else:
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT, node.url))
+                    new_nodes.append(TextNode(item_text, text_type, item_link))
+                    new_nodes.append(node)
+    return new_nodes
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    return split_nodes_items(old_nodes, TextType.IMAGE)
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    return split_nodes_items(old_nodes, TextType.LINK)
---
+++ src/test_split_nodes_items.py
@@ -0,0 +1,46 @@
+from unittest import main, TestCase
+
+from split_nodes_items import split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
+
+from textnode import TextType
+
+
+class TestSplitNodesItems(TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_nodes_links(self) -> None:
+        node = TextNode(
+            "This is text with a [link](https://example.com) and another [second link](https://example.org)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://example.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second link", TextType.LINK, "https://example.org"),
+            ],
+            new_nodes,
+        )
+
+
+if __name__ == "__main__":
+    main()
