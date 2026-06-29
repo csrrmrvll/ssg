ec92dc863fd562758d2678e09751c41b81e33c25
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-29 20:55:48 +0200

    feat: simplifying and removing redundant code

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -58,35 +58,27 @@ def get_opening_symbol(text_type: TextType) -> str:
 
 def split_nodes_items(old_nodes: list[TextNode], text_type: TextType) -> list[TextNode]:
     new_nodes: list[TextNode] = []
-    matches = extract_markdown_items(
-        " ".join([node.text for node in old_nodes if node.text_type == TextType.TEXT]),
-        text_type,
-    )
-    if not matches:
-        return old_nodes
     opening_symbol = get_opening_symbol(text_type)
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        remaining_text = old_node.text
+        original_text = old_node.text
+        matches = extract_markdown_items(original_text, text_type)
+        if not matches:
+            new_nodes.append(old_node)
+            continue
         for item_text, item_link in matches:
-            if f"{opening_symbol}{item_text}]({item_link})" in old_node.text:
-                split_nodes = remaining_text.split(
-                    f"{opening_symbol}{item_text}]({item_link})", 1
-                )
-                if len(split_nodes) < 2:
-                    continue  # Skip if the item is not found in the current node's text
-                first = split_nodes[0]
-                second = split_nodes[1]
-                if first != "":
-                    new_nodes.append(TextNode(first, TextType.TEXT, old_node.url))
-                new_nodes.append(TextNode(item_text, text_type, item_link))
-                remaining_text = second  # Update remaining_text to the second part for further splitting
-                if remaining_text == "":
-                    break  # No more text to process in this node
-        if remaining_text != "" and remaining_text not in [n.text for n in new_nodes]:
-            new_nodes.append(TextNode(remaining_text, TextType.TEXT, old_node.url))
+            sections = original_text.split(f"{opening_symbol}{item_text}]({item_link})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            first, second = sections
+            if first != "":
+                new_nodes.append(TextNode(first, TextType.TEXT))
+            new_nodes.append(TextNode(item_text, text_type, item_link))
+            original_text = second  # Update remaining_text to the second part for further splitting
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
 
