e6531bfa2939b86b3dad88a11cd31436967263c1
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-30 21:51:09 +0200

    feat: refactoring and simplifying

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,8 +1,13 @@
-from unittest import main, TestCase
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+import unittest
+from markdown_blocks import (
+    markdown_to_html_node,
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType,
+)
 
 
-class TestMarkdownToHTML(TestCase):
+class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
@@ -60,30 +65,104 @@ This is the same paragraph on a new line
         block = "paragraph"
         self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
-    def test_markdown_to_blocks_empty(self):
-        md = ""
-        blocks = markdown_to_blocks(md)
-        self.assertEqual(blocks, [])
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
 
-    def test_block_to_block_type(self):
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
         self.assertEqual(
-            block_to_block_type("This is a paragraph"), BlockType.PARAGRAPH
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
         )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
         self.assertEqual(
-            block_to_block_type("# This is a heading\n"), BlockType.HEADING
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
         )
-        self.assertEqual(block_to_block_type("```\nThis is code\n```"), BlockType.CODE)
-        self.assertEqual(block_to_block_type("> This is a quote"), BlockType.QUOTE)
-        self.assertEqual(block_to_block_type(">This is also a quote"), BlockType.QUOTE)
+
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and _more_ items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
         self.assertEqual(
-            block_to_block_type("- This is an unordered list item"),
-            BlockType.ULIST,
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
         )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
         self.assertEqual(
-            block_to_block_type("1. This is an ordered list item"),
-            BlockType.OLIST,
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
+
+    def test_code(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,5 @@
+from htmlnode import LeafNode
 from enum import Enum
-
-from htmlnode import HTMLNode, LeafNode
 
 
 class TextType(Enum):
@@ -21,36 +20,28 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, TextNode):
-            return NotImplemented
+    def __eq__(self, other: "TextNode") -> bool:
         return (
-            self.text == other.text
-            and self.text_type == other.text_type
+            self.text_type == other.text_type
+            and self.text == other.text
             and self.url == other.url
         )
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
-    @staticmethod
-    def text_node_to_html_node(text_node: "TextNode") -> LeafNode:
-        match text_node.text_type:
-            case TextType.TEXT:
-                return LeafNode(None, text_node.text)
-            case TextType.BOLD:
-                return LeafNode("b", text_node.text)
-            case TextType.ITALIC:
-                return LeafNode("i", text_node.text)
-            case TextType.CODE:
-                return LeafNode("code", text_node.text)
-            case TextType.LINK:
-                if not text_node.url:
-                    raise ValueError("Link text node must have a URL")
-                return LeafNode("a", text_node.text, props={"href": text_node.url})
-            case TextType.IMAGE:
-                if not text_node.url:
-                    raise ValueError("Image text node must have a URL")
-                return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
-            case _:
-                raise ValueError(f"Unsupported text type: {text_node.text_type}")
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
