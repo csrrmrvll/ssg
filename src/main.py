2a12f1adde7e76ac95b88e8eb6f8ef3de5387a3d
César Romero Avello <cesar.romero.avello@ing.com>
2026-06-30 22:18:19 +0200

    chore: removing debugging prints

--- src/main.py
+++ src/main.py
@@ -6,11 +6,9 @@ from textnode import TextNode, TextType
 
 def publish_content(origin: str = "static", destination: str = "public") -> None:
     try:
-        print(f"Publishing content from {origin} to {destination}")
         abs_origin = os.path.abspath(origin)
         abs_destination = os.path.abspath(destination)
-        print(f"Absolute origin: {abs_origin}")
-        print(f"Absolute destination: {abs_destination}")
+        print(f"Publishing content from '{abs_origin}' to '{abs_destination}'")
         if not os.path.exists(abs_origin):
             raise FileNotFoundError(f"Origin directory '{origin}' does not exist.")
         if not os.path.isdir(abs_origin):
@@ -22,17 +20,11 @@ def publish_content(origin: str = "static", destination: str = "public") -> None
         if os.path.exists(abs_destination):
             print(f"Destination directory '{destination}' already exists. Removing it.")
             shutil.rmtree(abs_destination)
-        # else:
-        #     print(f"Destination directory '{destination}' does not exist. Creating it.")
-        #     os.makedirs(abs_destination)
         if not os.access(abs_origin, os.R_OK):
             raise PermissionError(f"Origin directory '{origin}' is not readable.")
-        if not os.access(abs_destination, os.W_OK):
-            print(f"Destination directory '{destination}' is not writable.")
-
-        print(f"Copying content from '{origin}' to '{destination}'...")
+        print(f"Copying content...")
         shutil.copytree(abs_origin, abs_destination)
-        print(f"Content published successfully from '{origin}' to '{destination}'.")
+        print(f"Content published successfully '{abs_destination}'.")
         os.listdir(abs_destination)
     except Exception as e:
         print(f"An error occurred while publishing content: {e}")
