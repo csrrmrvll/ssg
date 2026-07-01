4229e954aec4fa3fac9aeb4e71abfa3784ef2963
César Romero Avello <cesar.romero.avello@ing.com>
2026-07-01 11:49:52 +0200

    feat: publishing the site in GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main() -> None:
+    argv = sys.argv
+    if len(argv) > 1:
+        basepath = argv[1]
+    else:
+        basepath = "/"
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -19,7 +25,8 @@ def main() -> None:
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
-main()
+if __name__ == "__main__":
+    main()
