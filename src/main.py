bc26d8f33d923b01bee7b20cb3359b12209b317e
César Romero Avello <cesar.romero.avello@ing.com>
2026-07-01 10:10:29 +0200

    feat: generating pages recursively

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main() -> None:
@@ -16,11 +18,8 @@ def main() -> None:
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page(
-        from_path="./content/index.md",
-        template_path="./template.html",
-        dest_path="./public/index.html",
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
