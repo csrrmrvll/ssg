7925d9a1515523aa64b027669f060c2ade088939
César Romero Avello <cesar.romero.avello@ing.com>
2026-07-01 09:09:25 +0200

    feat: generating html index from markdown

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,6 @@
+import os
+from markdown_blocks import markdown_to_html_node
+
 def extract_title(markdown: str) -> str:
     """
     Extracts the title from a markdown string. The title is defined as the first line that starts with a '#' character.
@@ -14,3 +17,18 @@ def extract_title(markdown: str) -> str:
         if stripped_line.startswith("# "):
             return stripped_line.lstrip("# ").strip()
     return ""
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
+    with open(from_path, "r", encoding="utf-8") as f:
+        markdown_content = f.read()
+    with open(template_path, "r", encoding="utf-8") as f:
+        template_content = f.read()
+    # html_content = template_content.replace("{{ title }}", title).replace("{{ content }}", markdown_content)
+    html_content = markdown_to_html_node(markdown_content).to_html()
+    title = extract_title(markdown_content)
+    html_page = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+    with open(dest_path, "w", encoding="utf-8") as f:
+        f.write(html_page)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
+from generate_page import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -14,6 +15,12 @@ def main() -> None:
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    generate_page(
+        from_path="./content/index.md",
+        template_path="./template.html",
+        dest_path="./public/index.html",
+    )
 
 
 main()
